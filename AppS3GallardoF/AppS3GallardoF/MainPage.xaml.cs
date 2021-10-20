f47778f4981f6e8110585cba248c23f2da4a4c0f
Fernanda Gallardo <Fernanda Gallardo@DESKTOP-6UVI768>
2021-10-19 19:23:28 -0500

    Agregar archivos de proyecto.

---
+++ AppS3GallardoF/AppS3GallardoF/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppS3GallardoF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                //almacenando en varibales lo que usuario ingresa
+                string usuario = txtUsuario.Text;
+                string clave = txtClave.Text;
+                //Permite abrir la ventana(ViewDos)
+                await Navigation.PushAsync(new ViewDos(usuario, clave));
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Mensaje de advertencia", ex.Message, "OK");
+            }
+
+        }
+    }
+}
